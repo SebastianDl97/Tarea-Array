4a15c46192818aa6e4b1059ebb635cfed1f9759b
Eddy Delgado Limones <sebaseldemente9443@outlook.com>
2019-01-06 15:44:06 -0500

    multiplicacion de matrices

--- TareaArrays/TareaArrays/Program.cs
+++ TareaArrays/TareaArrays/Program.cs
@@ -10,7 +10,33 @@ namespace TareaArrays
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("HOla");
+            int[,] A = { { 1, 1 }, { 1, 1 }, { 1, 1 } };
+            int[,] B = { { 2, 2 }, { 2, 2 }, { 2, 2 } };
+            int[,] resultado = new int[3, 3];
+            int c;
+            int suma = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                c = 0;
+                while (c < 3)
+                {
+
+                    for (int j = 0; j < 2; j++)
+
+                        suma = (suma) + ((A[i, j]) * (B[j, c]));
+                    resultado[i, c] = suma;
+                    c = c + 1;
+
+                }
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                    Console.WriteLine(resultado[i, j] + "\t");
+                Console.WriteLine();
+            }
+            Console.ReadKey();
         }
     }
+
 }
